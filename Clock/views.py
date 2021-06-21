a03096021b0f1bb1c695f2b92c9a35cf0f7c9f1a
katariyarohit <katariyarohit16@gmail.com>
2021-06-21 10:44:14 +0530

    first commit

---
+++ Clock/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from datetime import datetime
+
+# Create your views here.
+def clock(request):
+     now = datetime.now()
+     format = '%H:%M:%S %p'
+     current_time = now.strftime(format)
+     current_date = now.strftime('%d-%m-%Y')
+     context = {'time':current_time,'date':current_date}
+     return render(request,'clock.html',context)
